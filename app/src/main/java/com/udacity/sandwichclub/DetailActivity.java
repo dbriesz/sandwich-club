d5a1bc73918904994525834a7158f26f567ecbc9
David Riesz <dbriesz@users.noreply.github.com>
2018-04-27 11:47:31 -0700

    Add code for parsing JSON and populating UI

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.json.JSONException;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private ImageView mSandwichImage;
+    private TextView mSandwichOrigin;
+    private TextView mSandwichAlsoKnownAs;
+    private TextView mSandwichDescription;
+    private TextView mSandwichIngredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +45,12 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        Sandwich sandwich = null;
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -57,6 +71,10 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
-
+        mSandwichImage = (ImageView) findViewById(R.id.image_iv);
+        mSandwichOrigin = (TextView) findViewById(R.id.origin_tv);
+        mSandwichAlsoKnownAs = (TextView) findViewById(R.id.also_known_tv);
+        mSandwichDescription = (TextView) findViewById(R.id.description_tv);
+        mSandwichIngredients = (TextView) findViewById(R.id.ingredients_tv);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,32 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        JSONObject sandwichData = new JSONObject(json);
+        String mainName = String.valueOf(sandwichData.getJSONObject("main_name"));
+        List<String> alsoKnownAs = new ArrayList<>();
+        JSONArray alsoKnownAsData = sandwichData.getJSONArray("alsoKnownAs");
+        for (int i = 0; i < alsoKnownAsData.length(); i++) {
+            alsoKnownAs.add(alsoKnownAsData.getString(i));
+        }
+        String placeOfOrigin = String.valueOf(sandwichData.getJSONObject("placeOfOrigin"));
+        String description = String.valueOf(sandwichData.getJSONObject("description"));
+        String image = String.valueOf(sandwichData.getJSONObject("image"));
+        List<String> ingredients = new ArrayList<>();
+        JSONArray ingredientData = sandwichData.getJSONArray("ingredients");
+        for (int i = 0; i < ingredientData.length(); i++) {
+            alsoKnownAs.add(ingredientData.getString(i));
+        }
+
+        return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
     }
 }
